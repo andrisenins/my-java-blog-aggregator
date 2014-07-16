ee23f9af869c2644bbf5b7b2174ff7fcd67b5657
andrisenins <andrisenins@gmail.com>
2014-07-16 17:21:09 +0300

    New controllers and additional users page
    New page with users, changes for classic.jsp added User service and User
    controller and user.jsp

---
+++ src/main/java/com/andrisenins/jba/service/UserService.java
@@ -0,0 +1,21 @@
+package com.andrisenins.jba.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andrisenins.jba.entity.User;
+import com.andrisenins.jba.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+}
