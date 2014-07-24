23749a4046b9abfdeb515af8c576bac430b22639
andrisenins <andrisenins@gmail.com>
2014-07-24 16:34:44 +0300

    validate username
    validate if such username already exists

--- src/main/java/com/andrisenins/jba/entity/User.java
+++ src/main/java/com/andrisenins/jba/entity/User.java
@@ -3,6 +3,7 @@ package com.andrisenins.jba.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -14,6 +15,8 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 
+import com.andrisenins.jba.annotation.UniqueUsername;
+
 @Entity
 @Table(name="app_user")
 public class User {
@@ -23,6 +26,8 @@ public class User {
     private Integer id;
 
     @Size(min=3, message="Name must be at least 3 characters!")
+    @Column(unique=true)
+    @UniqueUsername(message="Such username already exists!")
     private String name;
     
     @Size(min=1, message="Invalid email address!")
